f49a92920d55e63cf361d1f2df7e4cf9e595d973
TaynaraTech <nascimento.taynaracristina@gmail.com>
2024-12-06 17:08:22 -0300

    Create exemplo.js

---
+++ exemplo.js
@@ -0,0 +1,6 @@
+let roupas= [
+    {id: 1, nome: "calcas cargo", preco: 69.99, tamanho: "G", estoque: 1586},
+    {id: 1, nome: "camisa", preco: 50.69, tamanho: "G", estoque: 1003},
+    {id: 1, nome: "body", preco: 45.88, tamanho: "M", estoque: 59},
+    {id: 1, nome: "moletom", preco: 78.99, tamanho: "G", estoque: 236}
+]
